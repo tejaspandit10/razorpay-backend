ab4af581246904eb6605f305d7d802eb9fc6e3f5
tejaspandit10 <tejasgp10@gmail.com>
2026-02-13 00:34:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -275,5 +275,5 @@ app.post("/verify-payment", async (req, res) => {
 // START SERVER
 ////////////////////////////////////////////////////
 app.listen(PORT, () => {
-  console.log(🚀 Backend running on port ${PORT});
+  console.log( `Backend running on port ${PORT}`);
 });
