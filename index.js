ce9c4b3faaeb1cadcb509c16e582304a50c4280d
tejaspandit10 <tejasgp10@gmail.com>
2026-02-13 15:39:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ app.use(express.json());
 const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
 
-console.log(prisma._baseDmmf.datamodel.models.find(m => m.name === "Payment"));
-
 const PORT = process.env.PORT || 5000;
 
 ////////////////////////////////////////////////////
